df760ebf976a0e2d0aab88577d6e84537d6da55d
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2017-11-23 21:26:32 +0100

    Add data description by pandas.

--- leastSquare.py
+++ leastSquare.py
@@ -5,6 +5,7 @@ import sklearn.linear_model as lm
 # Load data
 url = 'cars.csv'
 dat = pd.read_csv(url)
+print(dat.describe())
 y = dat['dist']
 X = dat[['speed']] # sklearn needs X to have 2 dim.
 
